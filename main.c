58f17d425cd2f2258d7a0aea68053d35ee858306
yyemms <105846058+yyemms@users.noreply.github.com>
2022-05-19 09:29:39 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,81 @@
+#include "functions.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(void) {
+
+	int odabir_zupanije;
+	
+	while (1) {
+		printf("1.Bjelovarsko-bilogorska županija \n");
+		printf("___________________________________\n");
+		printf("2.Brodsko-posavska županija\n");
+		printf("___________________________________\n");
+		printf("3.Dubrovačko-neretvanska županija\n");
+		printf("___________________________________\n");
+		printf("4.Istarska županija\n");
+		printf("___________________________________\n");
+		printf("5.Karlovačka županija \n");
+		printf("___________________________________\n");
+		printf("6.Koprivničko-križevačka županija\n");
+		printf("___________________________________\n");
+		printf("7.Krapinsko-zagorska županija\n");
+		printf("___________________________________\n");
+		printf("8.Ličko-senjska županija \n");
+		printf("___________________________________\n");
+		printf("9.Međimurska županija\n");
+		printf("___________________________________\n");
+		printf("10.Osječko-baranjska županija\n");
+		printf("___________________________________\n");
+		printf("11.Požeško-slavonska županija\n");
+		printf("___________________________________\n");
+		printf("12.Primorsko-goranska županija \n");
+		printf("___________________________________\n");
+		printf("13.Sisačko-moslavačka županija\n");
+		printf("___________________________________\n");
+		printf("14.Splitsko-dalmatinska županija\n");
+		printf("___________________________________\n");
+		printf("15.Šibensko-kninska županija\n");
+		printf("___________________________________\n");
+		printf("16.Varaždinska županija\n");
+		printf("___________________________________\n");
+		printf("17.Virovitičko-podravska županija\n");
+		printf("___________________________________\n");
+		printf("18.Vukovarsko-srijemska županija\n");
+		printf("___________________________________\n");
+		printf("19.Zadarska županija \n");
+		printf("___________________________________\n");
+		printf("20.Zagrebačka županija \n");
+		printf("___________________________________\n");
+		printf("21.Grad Zagreb \n");
+		printf("___________________________________\n");
+		printf("\n");
+		printf("Izaberite zupaniju za koju biste htjeli vidjeti planirane koncerte: ");
+
+
+
+
+	int odabir_opcije;
+	while (1) {				
+		printf("*********KUPOVINA ULAZNICA*********\n");
+		printf("___________________________________\n");
+		printf("     1.  Mjesto dogadaja       \n");
+		printf("____________________________________\n")
+		printf("     2.  Datum dogadaja       \n");
+		printf("____________________________________\n");
+		printf("     3.  Vrijeme dogadaja       \n");
+		printf("____________________________________\n");
+		printf("   4.  Kupovina ulaznica    \n");
+		printf("____________________________________\n");
+		printf("   5.  Rezervacija ulaznica    \n");
+		printf("____________________________________\n");
+		printf("   6. Pretrazivanje kosarice   \n");
+		printf("____________________________________\n");
+		printf("    7. Brisanje ulaznica     \n");
+		printf("____________________________________\n");
+		printf("  8. Izlazak iz izbornika  \n");
+		printf("____________________________________\n");
+		printf("\n");
+		printf("Izaberite opciju: ");
+		
