765a0a25cbdca72cfdb5468f0d1385fc53edbaf3
yyemms <105846058+yyemms@users.noreply.github.com>
2022-06-19 15:43:46 +0200

    Update and rename functions.h to funckcije.h

---
+++ funckcije.h
@@ -0,0 +1,14 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef FUNKCIJE_H
+#define FUNKCIJE_H
+
+
+void insert_person();
+void insert_ticket();
+void display_persons();
+void display_tickets();
+
+
+#endif // FUNKCIJE_H
+
